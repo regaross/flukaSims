827cc5742c9d8c0a45c3ed211d38e230fa2eecd6
Regan Ross <rross@laurentian.ca>
2023-01-25 15:31:32 -0400

    Added SLURM job script

---
+++ job_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --partition=shared
+#
+#SBATCH --job-name=fluka_simulation
+#SBATCH --output=output-%j.txt
+#SBATCH --error=output-%j.txt
+#
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10g
+#
+#SBATCH --time=0-10:00:00
+#
+#SBATCH --cpus 1
+
+cd /gpfs/slac/staas/fs1/g/exo/exo_data8/exo_data/users/rross/flukaSims
+
+singularity exec -B /gpfs fluka_nEXO.sif python runsim.py
