7e8c42c0a21030225aefa185e28ee91ce5ca2f5b
Regan Ross <rross@laurentian.ca>
2023-05-22 13:25:48 -0300

    remove unnecessary imports from init

--- simtools/__init__.py
+++ simtools/__init__.py
@@ -3,15 +3,8 @@
 #                                               }
 #################################################
 
-import numpy as np
-import h5py as h5
-import time
-import matplotlib.pyplot as plt
 from scipy import constants as sc
-from datetime import datetime
 
-plt.rcParams.update({'text.usetex' : True,
-        'font.family' : 'lmodern'})
 
 #################################################
 #                   CONSTANTS                    }
