086b46fba89dab16d60e4701f2b4638f42802b94
Regan Ross <rross@laurentian.ca>
2024-03-05 13:01:11 -0500

    added docker tools for running flair from Docker Container

---
+++ docker_flair/boot_docker_flair.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the IP address of the host
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+
+# Allow connections to X server from host
+xhost + $ip
+
+local_dir="./"
+
+# Run Docker container with necessary configurations
+docker run -it --rm  --name flair -e DISPLAY=$ip:0 -v /tmp/.X11-unix:/tmp/.X11-unix -v $local_dir:/shared_folder flair_image
+
